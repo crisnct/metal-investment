716865afbdae7f5b4d2675f85bb9cc09cb7de634
crisnct <39335855+crisnct@users.noreply.github.com>
2025-10-15 18:44:22 +0300

    Fixed problem about CSRF protection

--- src/main/java/com/investment/metal/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/investment/metal/infrastructure/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf
                 .csrfTokenRequestHandler(requestHandler)
                 .csrfTokenRepository(csrfTokenRepository)
-                .ignoringRequestMatchers("/login", "/userRegistration", "/validateAccount", "/resetPassword", "/changePassword")
+                .ignoringRequestMatchers("/login", "/userRegistration", "/validateAccount", "/resetPassword", "/changePassword", "/api/csrf-token")
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint()))
